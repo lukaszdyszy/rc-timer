cc389e7c8e828b6da4adc1ecd262607cb28613c3
lukaszdyszy <mruhol123@gmail.com>
2020-03-11 20:08:06 +0100

    timer working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Timer from './components/timer/timer.js';
 
 function App() {
+  const [times, updateTimes] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-content">
+        <div className="timer">
+          <Timer />
+        </div>
+        <div className="tools">
+          theme: white
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/timer/timer.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import './timer.css';
+
+let timerStart = false;
+const Timer = () => {
+    let [timerStatus, timerControl] = useState('neutral');
+    let [time, setTime] = useState(0);
+    let [beginTime, setBeginTime] = useState(0);
+
+    const runTime = () => {
+        if(timerStart){
+            setTime(Date.now()-beginTime);
+            setTimeout(runTime, 10);
+        }
+    }
+
+    const spaceDown = (e) => {
+        if(e.keyCode == 32){
+            if(timerStatus == 'neutral'){
+                timerControl('ready');
+            } else if(timerStatus == 'running'){
+                timerControl('stopped');
+            }
+        }
+    }
+
+    const spaceUp = (e) => {
+        if(e.keyCode == 32){
+            if(timerStatus == 'ready'){
+                setBeginTime(Date.now());
+                timerControl('running');
+            } else if(timerStatus == 'stopped'){
+                timerControl('neutral');
+            }
+        }
+    }
+
+    useEffect(() => {
+        if(timerStatus=='running'){
+            timerStart = true;
+            runTime();
+        } else {
+            timerStart = false;
+            if(timerStatus=='ready'){
+                setTime(0);
+            }
+        }
+    }, [timerStatus]);
+
+
+    const parseTime = () => {
+        let min = Math.floor(time / 60000);
+        let sec = Math.floor((time - min*60000) / 1000);
+        let ms  = Math.floor((time - min*60000 - sec*1000) / 10);
+
+
+        if(min < 1){min = '';}else{min = min+':';}
+        if(sec < 10){sec='0'+sec;}sec=sec+':';
+        if(ms < 10){ms='0'+ms;}
+
+        return min + sec + ms;
+    }
+
+    return (
+        <div className="timer-container">
+            <input type="text" id="time" onKeyDown={spaceDown} onKeyUp={spaceUp}/>
+            <label htmlFor="time" id="time-label">{ parseTime() }</label>
+        </div>
+    )
+}
+
+export default Timer;
