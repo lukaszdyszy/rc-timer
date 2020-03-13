146372687037eceb61e503df0af92019e3256cf1
lukaszdyszy <mruhol123@gmail.com>
2020-03-13 13:43:41 +0100

    average bug fixed

--- src/components/average.js
+++ src/components/average.js
@@ -1,11 +1,25 @@
 const average = (solves) => {
     if(solves.length >= 3){
-        solves.sort((a, b) => a-b);
-        let sum = 0;
-        for(let i=1; i<solves.length-1; i++){
-            sum += Math.floor(solves[i]/10)*10;
+        let dnfs = 0;
+        do{
+            if(solves.indexOf('DNF')!=-1){
+                dnfs++;
+                solves.splice(solves.indexOf('DNF'), 1);
+            }
+        }while(solves.indexOf('DNF')!=-1);
+        if(dnfs >= 2){
+            return 'DNF';
+        } else {
+            solves.sort((a, b) => a-b);
+            if(dnfs==1){
+                solves.unshift('dnf');
+            }
+            let sum = 0;
+            for(let i=1; i<solves.length-1; i++){
+                sum += Math.floor(solves[i]/10)*10;
+            }
+            return sum/(solves.length-2);
         }
-        return sum/(solves.length-2);
     } else {
         return 0;
     }
--- src/components/mean.js
+++ src/components/mean.js
@@ -1,10 +1,15 @@
 const mean = (solves) => {
     if(solves.length >= 1){
-        let sum = 0;
-        for(let i=0; i<solves.length; i++){
-            sum += Math.floor(solves[i]/10)*10;
+        if(solves.indexOf('DNF') != -1){
+            return 'DNF';
+        } else {
+            solves.sort((a, b) => a-b);
+            let sum = 0;
+            for(let i=1; i<solves.length-1; i++){
+                sum += Math.floor(solves[i]/10)*10;
+            }
+            return sum/(solves.length);
         }
-        return sum/(solves.length);
     } else {
         return 0;
     }
