72f3be63c04f8f347a8e5159234b32c80637e11e
lukaszdyszy <mruhol123@gmail.com>
2020-03-15 10:52:54 +0100

    info in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './header.css';
+import { FaGithub } from 'react-icons/fa';
 
 const Header = () => {
     return (
         <div className="header-container">
-            Header
+            <div className="menu"></div>
+            <div className="copyright">
+                <span className="copy">&copy; Łukasz Dyszy </span>
+                <a className="gh" target="blank" href="https://github.com/lukaszdyszy/rc-timer"><FaGithub size={28}/></a>
+            </div>
         </div>
     )
 }
